e3136a2eb77e4f5247d78608e72096385b865da0
Jonathan Boos <gameguy37@gmail.com>
2019-04-26 17:07:29 -0400

    Commit 1707

--- ActiveRecordLite-skeleton/lib/03_associatable.rb
+++ ActiveRecordLite-skeleton/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -20,13 +21,35 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      # debugger
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+
   end
+
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    
+  defaults = {
+      :foreign_key => "#{self_class_name}Id".underscore.to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+
   end
 end
 
@@ -47,4 +70,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
